92466135a839991eb0e52f0a998c026b8b8d2a81
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-03-02 14:20:49 +0300

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Ярослав Любиченко on 2.3.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
