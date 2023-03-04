8abc6f8955bf5029a5753834bac1dfcc00bf01b8
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-03-04 17:58:33 +0300

    Create DataStore.

---
+++ Contacts/Services.swift
@@ -0,0 +1,53 @@
+//
+//  Services.swift
+//  Contacts
+//
+//  Created by Ярослав Любиченко on 2.3.2023.
+//
+
+class DataStore {
+    let names = [
+        "John",
+        "Sharon",
+        "Aaron",
+        "Steven",
+        "Nicola",
+        "Ted",
+        "Bruce",
+        "Carl",
+        "Allan"
+    ]
+    let surnames = [
+        "Smith",
+        "Butler",
+        "Isaacson",
+        "Pennyworth",
+        "Jankin",
+        "Robertson",
+        "Black",
+        "Williams",
+        "Murphy"
+    ]
+    let emails = [
+        "first@mail.ru",
+        "second@mail.ru",
+        "third@mail.ru",
+        "fourth@mail.ru",
+        "fifth@mail.ru",
+        "sixth@mail.ru",
+        "seventh@mail.ru",
+        "eigth@mail.ru",
+        "ninth@mail.ru"
+    ]
+    let phoneNumbers = [
+    "79891234567",
+    "79897654321",
+    "79895553333",
+    "79897771298",
+    "79890018765",
+    "79896741209",
+    "79890012312",
+    "79896664422",
+    "79899994321"
+    ]
+}
