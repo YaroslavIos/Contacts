bbc0d0445a03c929503130825ebf2e3ad151d9b3
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-03-04 17:58:56 +0300

    Create model.

---
+++ Contacts/Person.swift
@@ -0,0 +1,72 @@
+//
+//  Person.swift
+//  Contacts
+//
+//  Created by Ярослав Любиченко on 2.3.2023.
+//
+
+struct Person {
+    let name: String
+    let surname: String
+    let email: String
+    let phoneNumber: String
+    
+    static func getContact() -> [Person] {
+        [
+        Person(
+            name: DataStore.init().names.randomElement() ?? "",
+            surname: DataStore.init().surnames.randomElement() ?? "",
+            email: DataStore.init().emails.randomElement() ?? "",
+            phoneNumber: DataStore.init().phoneNumbers.randomElement() ?? ""
+        ),
+        Person(
+            name: DataStore.init().names.randomElement() ?? "",
+            surname: DataStore.init().surnames.randomElement() ?? "",
+            email: DataStore.init().emails.randomElement() ?? "",
+            phoneNumber: DataStore.init().phoneNumbers.randomElement() ?? ""
+        ),
+        Person(
+            name: DataStore.init().names.randomElement() ?? "",
+            surname: DataStore.init().surnames.randomElement() ?? "",
+            email: DataStore.init().emails.randomElement() ?? "",
+            phoneNumber: DataStore.init().phoneNumbers.randomElement() ?? ""
+        ),
+        Person(
+            name: DataStore.init().names.randomElement() ?? "",
+            surname: DataStore.init().surnames.randomElement() ?? "",
+            email: DataStore.init().emails.randomElement() ?? "",
+            phoneNumber: DataStore.init().phoneNumbers.randomElement() ?? ""
+        ),
+        Person(
+            name: DataStore.init().names.randomElement() ?? "",
+            surname: DataStore.init().surnames.randomElement() ?? "",
+            email: DataStore.init().emails.randomElement() ?? "",
+            phoneNumber: DataStore.init().phoneNumbers.randomElement() ?? ""
+        ),
+        Person(
+            name: DataStore.init().names.randomElement() ?? "",
+            surname: DataStore.init().surnames.randomElement() ?? "",
+            email: DataStore.init().emails.randomElement() ?? "",
+            phoneNumber: DataStore.init().phoneNumbers.randomElement() ?? ""
+        ),
+        Person(
+            name: DataStore.init().names.randomElement() ?? "",
+            surname: DataStore.init().surnames.randomElement() ?? "",
+            email: DataStore.init().emails.randomElement() ?? "",
+            phoneNumber: DataStore.init().phoneNumbers.randomElement() ?? ""
+        ),
+        Person(
+            name: DataStore.init().names.randomElement() ?? "",
+            surname: DataStore.init().surnames.randomElement() ?? "",
+            email: DataStore.init().emails.randomElement() ?? "",
+            phoneNumber: DataStore.init().phoneNumbers.randomElement() ?? ""
+        ),
+        Person(
+            name: DataStore.init().names.randomElement() ?? "",
+            surname: DataStore.init().surnames.randomElement() ?? "",
+            email: DataStore.init().emails.randomElement() ?? "",
+            phoneNumber: DataStore.init().phoneNumbers.randomElement() ?? ""
+        )
+        ]
+    }
+}
